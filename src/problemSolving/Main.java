86442c2e310a2c3f51489cd340d16c2858a9737f
hamin <ggamini7@gmail.com>
2022-08-02 23:18:49 +0900

    first commit

---
+++ src/problemSolving/Main.java
@@ -0,0 +1,15 @@
+package problemSolving;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        System.setIn(new FileInputStream("/Users/hamin/eclipse-workspace/BaekjoonOnlineJudge/src/problemSolving/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine())
+    }
+}
