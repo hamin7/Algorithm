61a641ccbd92553e0ee6797ee0c4f078fa4267c8
hamin <ggamini7@gmail.com>
2022-08-24 23:16:40 +0900

    edit 백준 2115 갤러리

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -8,13 +8,18 @@ public class Main {
     static int M;
     static int N;
     static Boolean[][] gallery;
+    static int res = 0;
 
     public static void main(String[] args) throws IOException {
-        input();
-        func();
+        inputSize();
+        upperSearch();
+        bottomSearch();
+        leftSearch();
+        rightSearch();
+        printResult();
     }
 
-    static void input() throws IOException {
+    static void inputSize() throws IOException {
         System.setIn(new FileInputStream("/Users/hamin/eclipse-workspace/BaekjoonOnlineJudge/src/problemSolving/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] size = br.readLine().split(" ");
@@ -34,9 +39,7 @@ public class Main {
         }
     }
 
-    static void func() {
-        int res = 0;
-
+    static void upperSearch() {
         // 윗벽 탐색
         for (int k = 1; k < M - 1; k++) {
             int cnt = 0;
@@ -50,7 +53,9 @@ public class Main {
             }
             res += cnt / 2;
         }
+    }
 
+    static void bottomSearch() {
         // 아랫벽 탐색
         for (int k = 1; k < M - 1; k++) {
             int cnt = 0;
@@ -64,21 +69,25 @@ public class Main {
             }
             res += cnt / 2;
         }
+    }
 
+    static void leftSearch() {
         // 왼벽 탐색
         for (int k = 1; k < N - 1; k++) {
             int cnt = 0;
             for (int l = 1; l < M - 1; l++) {
                 if (gallery[l][k] && !gallery[l][k - 1])
                     cnt++;
-                else{
+                else {
                     res += cnt / 2;
                     cnt = 0;
                 }
             }
             res += cnt / 2;
         }
+    }
 
+    static void rightSearch() {
         // 오른벽 탐색
         for (int k = 1; k < N - 1; k++) {
             int cnt = 0;
@@ -93,6 +102,9 @@ public class Main {
             res += cnt / 2;
         }
 
+    }
+
+    static void printResult() {
         System.out.println(res);
     }
 }
