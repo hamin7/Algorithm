dd24d231e80a9a1d38a5a9fbfd6e0caed89fa769
hamin <ggamini7@gmail.com>
2022-09-14 02:04:52 +0900

    edit 백준 15980

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,8 +1,7 @@
 package problemSolving;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -12,7 +11,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         input();
-        print(removeOneBird(inputBirdList, 0));
+        HashMap<Integer, Integer> removedBirdAndMaxDisturb = getMinimumDisturb();
+
+        // Comparator 정의
+        Comparator<Map.Entry<Integer, Integer>> comparator = new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
+                return e1.getValue().compareTo(e2.getValue());
+            }
+        };
+
+        // Min Value의 key, value
+        Map.Entry<Integer, Integer> minEntry = Collections.min(removedBirdAndMaxDisturb.entrySet(), comparator);
+        print(minEntry);
     }
 
     static void input() throws IOException {
@@ -43,14 +54,50 @@ public class Main {
         }
     }
 
-    static ArrayList<ArrayList> removeOneBird(ArrayList<ArrayList> birdList, int i) {
-        birdList.remove(i);
-        return birdList;
+    static ArrayList<ArrayList> removeOneBird(ArrayList<ArrayList> birdList, int removeIndex) {
+        ArrayList<ArrayList> tempList = new ArrayList<>();
+        for (int i = 0; i < birdList.size(); i++) {
+            tempList.add(birdList.get(i));
+        }
+        tempList.remove(removeIndex);
+        return tempList;
     }
 
-    static void print(ArrayList<ArrayList> list) {
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).toString());
+    static HashMap<Integer, Integer> getMinimumDisturb() {
+        HashMap<Integer, Integer> removedBirdAndMaxDisturb = new HashMap<>();
+        for (int removedIndex = 0; removedIndex < inputBirdList.size(); removedIndex++) {
+            // i번째 새를 제거
+            ArrayList<ArrayList> removedOneBird = removeOneBird(inputBirdList, removedIndex);
+            int maxDisturb = 0;
+            if (removedOneBird.size() != 0) {
+                maxDisturb = getMaxDisturbSecond(removedOneBird);
+            }
+            removedBirdAndMaxDisturb.put(removedIndex+1, maxDisturb);          // 새번호 = index +1
         }
+        return removedBirdAndMaxDisturb;
+    }
+
+    static int getMaxDisturbSecond(ArrayList<ArrayList> list) {
+        int maxDisturbSize = 0;
+        ArrayList<Integer> disturbAtSecondList = new ArrayList<>();
+        int disturbAtSecond = 0;
+        for (int second = 0; second < list.get(0).size(); second++) {
+            for (int birdNum = 0; birdNum < list.size(); birdNum++) {
+                disturbAtSecond += Integer.parseInt(list.get(birdNum).get(second).toString());
+            }
+            if (disturbAtSecond < 0) {
+                //음수이면 절대값 넣기
+                disturbAtSecondList.add(disturbAtSecond*(-1));
+            } else {
+                disturbAtSecondList.add(disturbAtSecond);
+            }
+        }
+        maxDisturbSize = Collections.max(disturbAtSecondList);
+        return maxDisturbSize;
+    }
+
+    static void print(Map.Entry<Integer, Integer> minEntry) {
+        System.out.println(minEntry.getKey());
+        System.out.println(minEntry.getValue());
     }
 }
