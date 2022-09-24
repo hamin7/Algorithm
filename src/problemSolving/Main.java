61078736c6f6c594c4a77e4b0d2f6a10d6173801
hamin <ggamini7@gmail.com>
2022-09-24 14:43:19 +0900

    add kakao 2023 #1

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -6,78 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int N = 5;
-        int[][] road = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
-        int K = 3;
-        System.out.println(s.solution(N, road, K));
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+        s.solution(today, terms, privacies);
     }
 }
 
 class Solution {
-    public int solution(int N, int[][] road, int K) {
-        ArrayList<ArrayList<Path>> townLink = mkTownLink(N, road);
-        int[] visit = mkMaxVisitArray(N);
-        Queue<Path> linkFromStart = mkLinkFromStart(townLink);
-        visit = bfs(linkFromStart, visit, townLink);
-        return cntAvailableTwn(visit, K);
-    }
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        HashMap<Character, Integer> temrsMap = mkTermsMap(terms);
 
-    public ArrayList<ArrayList<Path>> mkTownLink(int N, int[][] road) {
-        ArrayList<ArrayList<Path>> list = new ArrayList<>();
+//        String[] expireDates = calculateExpireDates(terms, privacies);
 
-        for (int i = 0; i <= N; i++) {
-            list.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < road.length; i++) {
-            list.get(road[i][0]).add(new Path(road[i][0], road[i][1], road[i][2]));
-            list.get(road[i][1]).add(new Path(road[i][1], road[i][0], road[i][2]));
-        }
-
-        return list;
-    }
-
-    public int[] mkMaxVisitArray(int N) {
-        int[] visit = new int[N+1];
-        for (int i = 2; i < visit.length; i++) {
-            visit[i] = Integer.MAX_VALUE;       // 방문 배열을 모두 max 값으로 갱신.
-        }
-        return visit;
-    }
-
-    public Queue<Path> mkLinkFromStart(ArrayList<ArrayList<Path>> townLink) {
-        Queue<Path> linkFromStart = new LinkedList<>();
-        linkFromStart.addAll(townLink.get(1));      // 1번 마을에서 갈수있는 마을 정보 모두 큐에 담음.
-        return linkFromStart;
-    }
-
-    public int[] bfs(Queue<Path> linkFromStart, int[] visit, ArrayList<ArrayList<Path>> townLink) {
-        while(!linkFromStart.isEmpty()) {
-            // bfs
-            Path path = linkFromStart.poll();
-            if (visit[path.end] <= visit[path.start] + path.time)
-                continue;
-            visit[path.end] = visit[path.start] + path.time;
-            linkFromStart.addAll(townLink.get(path.end));
-        }
-        return visit;
-    }
-
-    public int cntAvailableTwn(int[] visit, int K) {
-        int answer = 1;     // 1번마을 무조건 갈 수 있음.
-        for (int i = 2; i < visit.length; i++) {
-            if (visit[i] <= K)
-                answer++;
-        }
+        int[] answer = {};
         return answer;
     }
 
-    static class Path {
-        int start, end, time;
-        Path(int start, int end, int time) {
-            this.start = start;
-            this.end = end;
-            this.time = time;
+    static HashMap<Character, Integer> mkTermsMap(String[] terms) {
+        HashMap<Character, Integer> termsMap = new HashMap<>();
+        for (int i = 0; i < terms.length; i++) {
+            String[] term = terms[i].split(" ");
+            Character termKind = term[0].charAt(0);
+            Integer validity = Integer.parseInt(term[1]);
+            termsMap.put(termKind, validity);
         }
+        return termsMap;
     }
+
+//    static String[] calculateExpireDates(String[] terms, String[] privacies) {
+//
+//    }
 }
