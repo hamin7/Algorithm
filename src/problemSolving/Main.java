2ba52b18568862d59d0ed0ab699e633ff7aa451c
hamin <ggamini7@gmail.com>
2022-11-14 02:43:59 +0900

    Cos pro 체스의 나이트

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,39 +1,17 @@
 package problemSolving;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Main {
-    public int solution(int n) {
-        int[][] soyongdoli = new int[n][n];
-        int i = 0, j = 0;
-        int dir = 0;	// dir 0 : →, dir 1 : ↓, dir 2 : ←, dir 3 : ↑
-        int num = 1;
-
-        while (true) {
-            soyongdoli[i][j] = num;
-
-            if(dir==0 && (j==n-1 || j<n-1 && soyongdoli[i][j+1]!=0))
-                dir++;
-            else if(dir==1 && (i==n-1 || i<n-1 && soyongdoli[i+1][j]!=0))
-                dir++;
-            else if(dir==2 && (j==0 || j>0 && soyongdoli[i][j-1]!=0))
-                dir++;
-            else if(dir==3 && (i==0 || i>0 && soyongdoli[i-1][j]!=0))
-                dir=0;
-
-            if(dir==0)j++;
-            else if(dir==1)i++;
-            else if(dir==2)j--;
-            else if(dir==3)i--;
-
-            if(num == Math.pow(n,2)) break;
-            num++;
-        }
-
+class Main {
+    int[] dx = {-1, -2, -1, -2, 1, 2, 1, 2};
+    int[] dy = {-2, -1, 2, 1, -2, -1, 2, 1};
+    public int solution(String pos) {
         int answer = 0;
-        for (int index = 0; index < n; index++) {
-            answer += soyongdoli[index][index];
+        int col = (int)(pos.charAt(0) - 'A');
+        int row = 8 - (int)(pos.charAt(1) - '0');
+        for (int i = 0; i < 8; i++) {
+            int nx = row + dx[i];
+            int ny = col + dy[i];
+            if (nx>=0 && nx<8 && ny>=0 && ny<8)
+                answer++;
         }
         return answer;
     }
@@ -41,14 +19,10 @@ public class Main {
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int n1 = 3;
-        int ret1 = sol.solution(n1);
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        String pos = "A7";
+        int ret = sol.solution(pos);
 
-        int n2 = 2;
-        int ret2 = sol.solution(n2);
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
     }
 }
