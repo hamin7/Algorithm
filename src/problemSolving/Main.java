0ded79feb51856d1741bbb44f3bc64b868da49f0
hamin <ggamini7@gmail.com>
2022-11-14 14:21:22 +0900

    Cos pro 지하철 기다리기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,42 +1,42 @@
 package problemSolving;
 
 class Main {
-    interface Book {
-        public int getRentalPrice(int day);
+    public int func_a(String times){
+        int hour = Integer.parseInt(times.substring(0, 2));
+        int minute = Integer.parseInt(times.substring(3));
+        return hour*60 + minute;
+    }
+    public int solution(String[] subwayTimes, String currentTime) {
+        int currentMinute = func_a(currentTime);
+        int INF = 1000000000;
+        int answer = INF;
+        for(int i = 0; i < subwayTimes.length; ++i){
+            int subwayMinute = func_a(subwayTimes[i]);
+            if(subwayMinute>=currentMinute){
+                answer = subwayMinute - currentMinute;
+                break;
+            }
+        }
+        if(answer == INF)
+            return -1;
+        return answer;
     }
 
-    class ComicBook implements Book {
-        public int getRentalPrice(int day) {
-            int cost = 500;
-            day -= 2;
-            if (day > 0)
-                cost += day * 200;
-            return cost;
-        }
-    }
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        String[] subwayTimes1 = {"05:31", "11:59", "13:30", "23:32"};
+        String currentTime1 = "12:00";
+        int ret1 = sol.solution(subwayTimes1, currentTime1);
 
-    class Novel implements Book {
-        public int getRentalPrice(int day) {
-            int cost = 1000;
-            day -= 3;
-            if (day > 0)
-                cost += day * 300;
-            return cost;
-        }
-    }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
 
-    public int solution(String[] bookTypes, int day) {
-        Book[] books = new Book[50];
-        int length = bookTypes.length;
-        for (int i = 0; i < length; ++i) {
-            if (bookTypes[i].equals("comic"))
-                books[i] = new ComicBook();
-            else if (bookTypes[i].equals("novel"))
-                books[i] = new Novel();
-        }
-        int totalPrice = 0;
-        for (int i = 0; i < length; ++i)
-            totalPrice += books[i].getRentalPrice(day);
-        return totalPrice;
+        String[] subwayTimes2 = {"14:31", "15:31"};
+        String currentTime2 = "15:31";
+        int ret2 = sol.solution(subwayTimes2, currentTime2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
