ef811e439a9d4b33bbcb698b8e6f6b46c0c30bc2
hamin <ggamini7@gmail.com>
2022-11-14 14:19:53 +0900

    Cos pro 도서 대여점 운영

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,28 +1,42 @@
 package problemSolving;
 
 class Main {
-    int[] dx = {-1, -2, -1, -2, 1, 2, 1, 2};
-    int[] dy = {-2, -1, 2, 1, -2, -1, 2, 1};
-    public int solution(String pos) {
-        int answer = 0;
-        int col = (int)(pos.charAt(0) - 'A');
-        int row = 8 - (int)(pos.charAt(1) - '0');
-        for (int i = 0; i < 8; i++) {
-            int nx = row + dx[i];
-            int ny = col + dy[i];
-            if (nx>=0 && nx<8 && ny>=0 && ny<8)
-                answer++;
-        }
-        return answer;
+    interface Book {
+        public int getRentalPrice(int day);
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
-    public static void main(String[] args) {
-        Main sol = new Main();
-        String pos = "A7";
-        int ret = sol.solution(pos);
+    class ComicBook implements Book {
+        public int getRentalPrice(int day) {
+            int cost = 500;
+            day -= 2;
+            if (day > 0)
+                cost += day * 200;
+            return cost;
+        }
+    }
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    class Novel implements Book {
+        public int getRentalPrice(int day) {
+            int cost = 1000;
+            day -= 3;
+            if (day > 0)
+                cost += day * 300;
+            return cost;
+        }
+    }
+
+    public int solution(String[] bookTypes, int day) {
+        Book[] books = new Book[50];
+        int length = bookTypes.length;
+        for (int i = 0; i < length; ++i) {
+            if (bookTypes[i].equals("comic"))
+                books[i] = new ComicBook();
+            else if (bookTypes[i].equals("novel"))
+                books[i] = new Novel();
+        }
+        int totalPrice = 0;
+        for (int i = 0; i < length; ++i)
+            totalPrice += books[i].getRentalPrice(day);
+        return totalPrice;
     }
 }
