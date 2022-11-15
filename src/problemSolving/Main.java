d67f227d98ad39f3a4e82233e15646540c7c5d51
hamin <ggamini7@gmail.com>
2022-11-15 20:04:00 +0900

    Cos pro 로봇을 움직여주세요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,29 +1,31 @@
 package problemSolving;
 
+import java.util.*;
+
 class Main {
-    public int solution(int[] arr) {
-        int increaseCnt = 0;
-        int curMax = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i-1] < arr[i]) {
-                increaseCnt++;
-                if (curMax < increaseCnt) {
-                    curMax = increaseCnt;
-                }
-            } else {
-                increaseCnt = 0;
-            }
+    public int[] solution(String commands) {
+        int[] answer = {0, 0};
+        for (int i = 0; i < commands.length(); i++) {
+            if (commands.charAt(i) == 'L')
+                answer[0]--;
+            else if (commands.charAt(i) == 'R')
+                answer[0]++;
+            else if (commands.charAt(i) == 'U')
+                answer[1]++;
+            else if (commands.charAt(i) == 'D')
+                answer[1]--;
         }
-        return curMax+1;
+
+        return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] arr = {3, 1, 2, 4, 5, 1, 2, 2, 3, 4};
-        int ret = sol.solution(arr);
+        String commands = "URDDL";
+        int[] ret = sol.solution(commands);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
     }
 }
