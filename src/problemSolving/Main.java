88ecec1bbfd4916223b6d1a6b37942ed1f3436a7
hamin <ggamini7@gmail.com>
2022-11-15 21:16:26 +0900

    Cos pro 규칙에 맞는 배열 구하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -3,25 +3,31 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public int solution(int money) {
-        int coin[] = {10, 50, 100, 500, 1000, 5000, 10000, 50000};
-        int counter = 0;
-        int idx = coin.length - 1;
-        while (money > 0){
-            counter += money/coin[idx];
-            money %= coin[idx];
-            idx -= 1;
+    public int[] solution(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        int idx = 0;
+        int[] answer = new int[arr.length];
+        while(left <= right){
+            if(idx % 2 == 0){
+                answer[idx] = arr[left];
+                left += 1;
+            }
+            else{
+                answer[idx] = arr[right];
+                right -= 1;
+            }
+            idx += 1;
         }
-        return counter;
+        return answer;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. main 메소드는 잘못된 부분이 없으니, solution 메소드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int money = 2760;
-        int ret = sol.solution(money);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] ret = sol.solution(arr);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
     }
 }
