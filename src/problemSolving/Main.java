dc6f5dd0bfe5c6bc0c65a4da85f72adef272532b
hamin <ggamini7@gmail.com>
2022-11-15 22:09:21 +0900

    Cos pro 비밀번호 검사

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -3,31 +3,34 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public int[] solution(int[] arr) {
-        int left = 0, right = arr.length - 1;
-        int idx = 0;
-        int[] answer = new int[arr.length];
-        while(left <= right){
-            if(idx % 2 == 0){
-                answer[idx] = arr[left];
-                left += 1;
-            }
-            else{
-                answer[idx] = arr[right];
-                right -= 1;
-            }
-            idx += 1;
+    public boolean solution(String password) {
+
+        int length = password.length();
+        for(int i = 0; i < length - 2; ++i){
+            int firstCheck = password.charAt(i + 1) - password.charAt(i);
+            int secondCheck = password.charAt(i + 2) - password.charAt(i+1);
+            if(firstCheck == secondCheck && (firstCheck == 1 || firstCheck == -1))
+                return false;
         }
-        return answer;
+        return true;
     }
+
+
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. main 메소드는 잘못된 부분이 없으니, solution 메소드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        int[] ret = sol.solution(arr);
+        String password1 = "cospro890";
+        boolean ret1 = sol.solution(password1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+
+        String password2 = "cba323";
+        boolean ret2 = sol.solution(password2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2+ " 입니다.");
     }
 }
