73a9c62ea24cfbc0d38455d8a8442840ab607c6a
hamin <ggamini7@gmail.com>
2022-11-15 20:18:49 +0900

    Cos pro 거스름돈 구하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -3,29 +3,25 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public int[] solution(String commands) {
-        int[] answer = {0, 0};
-        for (int i = 0; i < commands.length(); i++) {
-            if (commands.charAt(i) == 'L')
-                answer[0]--;
-            else if (commands.charAt(i) == 'R')
-                answer[0]++;
-            else if (commands.charAt(i) == 'U')
-                answer[1]++;
-            else if (commands.charAt(i) == 'D')
-                answer[1]--;
+    public int solution(int money) {
+        int coin[] = {10, 50, 100, 500, 1000, 5000, 10000, 50000};
+        int counter = 0;
+        int idx = coin.length - 1;
+        while (money > 0){
+            counter += money/coin[idx];
+            money %= coin[idx];
+            idx -= 1;
         }
-
-        return answer;
+        return counter;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        String commands = "URDDL";
-        int[] ret = sol.solution(commands);
+        int money = 2760;
+        int ret = sol.solution(money);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
     }
 }
