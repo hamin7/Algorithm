c3e21ed931587e3992a973e242c44419ca8b367a
hamin <ggamini7@gmail.com>
2022-11-17 23:25:41 +0900

    Cos pro 소수의 합을 표현하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,47 +1,45 @@
 package problemSolving;
 
-import java.util.*;
+import java.util.ArrayList;
 
 class Main {
-    public int solution(String s1, String s2) {
-        int shortLength = 0;
-        boolean s1Short = true;
-        if (s1.length() >= s2.length()) {
-            shortLength = s2.length();
-            s1Short = false;
-        } else {
-            shortLength = s1.length();
-        }
-
-        int sameLength = shortLength;
-        if (s1Short) {
-            while (true) {
-                if (s1.substring(shortLength - sameLength, s1.length()).equals(s2.substring(0, sameLength))) {
+    public int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        primes.add(2);
+        for (int i = 3; i <= n; i += 2) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++)
+                if (i % j == 0){
+                    isPrime = false;
                     break;
                 }
-                sameLength--;
-            }
-        } else {
-            while (true) {
-                if (s2.substring(shortLength - sameLength, s1.length()).equals(s2.substring(0, sameLength))) {
-                    break;
-                }
-                sameLength--;
-            }
+            if (isPrime)
+                primes.add(i);
         }
 
-        int answer = s1.length() + s2.length() - sameLength;
+        int primeLen = primes.size();
+        for (int i = 0; i < primeLen - 2; i++)
+            for (int j = i + 1; j < primeLen - 1; j++)
+                for (int k = j + 1; k < primeLen; k++)
+                    if (primes.get(i) + primes.get(j) + primes.get(k) == n)
+                        answer++;
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        String s1 = new String("ababc");
-        String s2 = new String("abcdab");
-        int ret = sol.solution(s1, s2);
+        int n1 = 33;
+        int ret1 = sol.solution(n1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+        int n2 = 9;
+        int ret2 = sol.solution(n2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
