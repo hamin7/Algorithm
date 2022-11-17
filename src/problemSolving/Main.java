e4478b1f79f8fc8427c9472aea798d570363166f
hamin <ggamini7@gmail.com>
2022-11-18 00:44:11 +0900

    Cos pro 선풍기를 몇대 사야 하나요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -5,36 +5,32 @@ import java.util.ArrayList;
 import java.util.*;
 
 class Main {
-    public int[] solution(int k) {
-        int[] kaprekarArr = new int[k];
-        int count = 0;
-        for (int i = 1; i <= k; i++) {
-            long squareNum = i * i;
-            long divisor = 10;
-            while (squareNum / divisor != 0) {
-                long front = squareNum / divisor;
-                long back = squareNum % divisor;
-                divisor *= 10;
-                if (back != 0 && front != 0)
-                    if (front + back == i) {
-                        kaprekarArr[count] = i;
-                        count++;
-                    }
-            }
+    public int solution(int k, int[] student) {
+        int answer = 0;
+        for(int i = 0; i < student.length; i++){
+            student[i] -= 4*k;
+            if(student[i] <= 0)
+                continue;
+            answer += (student[i] + k - 1) / k;
         }
-        int[] answer = new int[count];
-        for (int i = 0; i < count; i++)
-            answer[i] = kaprekarArr[i];
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. main 메소드는 잘못된 부분이 없으니, solution 메소드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int k = 500;
-        int[] ret = sol.solution(k);
+        int k1 = 1;
+        int[] student1 = {4, 4, 4, 4};
+        int ret1 = sol.solution(k1, student1);
+
+// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+        int k2 = 3;
+        int[] student2 = {15, 17, 19, 10, 23};
+        int ret2 = sol.solution(k2, student2);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
