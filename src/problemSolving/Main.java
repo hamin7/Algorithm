76de0ab3a93cb3d578d968b9cd18c589dde4d4e4
hamin <ggamini7@gmail.com>
2022-11-18 22:28:21 +0900

    Cos pro PRG스토리

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -3,38 +3,79 @@ package problemSolving;
 import java.util.Arrays;
 
 class Main {
-    public int power(int base, int exponent) {
-        int val = 1;
-        for (int i = 0; i < exponent; i++)
-            val *= base;
-        return val;
-    }
-    public int[] solution(int k) {
-        int range = power(10, k);
-        int[] answer = new int[range];
-        int count = 0;
-        for (int i = range / 10; i < range; i++) {
-            int current = i;
-            int calculated = 0;
-            while (current != 0) {
-                calculated += power(current%10, k);
-                current /= 10;
-            }
-            if (calculated == i)
-                answer[count++] = i;
+    class Unit {
+        public int HP;
+        public Unit() {
+            this.HP = 1000;
         }
+        public void underAttack(int damage) { }
+    }
 
-        int[] ret = new int[count];
-        for (int i = 0; i < count; i++)
-            ret[i] = answer[i];
-        return ret;
+    class Monster extends Unit {
+        public int attackPoint;
+        public Monster(int attackPoint) {
+            this.attackPoint = attackPoint;
+        }
+        public void underAttack(int damage) {
+            this.HP -= damage;
+        }
+        public int attack() {
+            return attackPoint;
+        }
+    }
+
+    class Warrior extends Unit {
+        public int attackPoint;
+        public Warrior(int attackPoint) {
+            this.attackPoint = attackPoint;
+        }
+        public void underAttack(int damage) {
+            this.HP -= damage;
+        }
+        public int attack() {
+            return attackPoint;
+        }
+    }
+
+    class Healer extends Unit {
+        public int healingPoint;
+        public Healer(int healingPoint) {
+            this.healingPoint = healingPoint;
+        }
+        public void underAttack(int damage) {
+            this.HP -= damage;
+        }
+        public void healing(Unit unit) {
+            unit.HP += healingPoint;
+        }
+    }
+    public int[] solution(int monsterAttackPoint, int warriorAttackPoint, int healingPoint) {
+        Monster monster = new Monster(monsterAttackPoint);
+        Warrior warrior = new Warrior(warriorAttackPoint);
+        Healer healer = new Healer(healingPoint);
+
+        //전사가 몬스터를 한 번 공격
+        monster.underAttack(warrior.attack());
+        //몬스터가 전사를 한 번 공격
+        warrior.underAttack(monster.attack());
+        //몬스터가 힐러를 한 번 공격
+        healer.underAttack(monster.attack());
+        //힐러가 전사의 체력을 한 번 회복
+        healer.healing(warrior);
+        //힐러가 몬스터의 체력을 한 번 회복
+        healer.healing(monster);
+
+        int[] answer = {monster.HP, warrior.HP, healer.HP};
+        return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int k = 3;
-        int[] ret = sol.solution(k);
+        int monsterAttackPoint = 100;
+        int warriorAttackPoint = 90;
+        int healingPoint = 30;
+        int[] ret = sol.solution(monsterAttackPoint, warriorAttackPoint, healingPoint);
 
         // 실행] 버튼을 누르면 출력값을 볼 수 있습니다.
         System.out.printf("solution 메소드의 반환 값은 ");
