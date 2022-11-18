4c53fb71ff4380d8d393df23f6500b990f44d6ad
hamin <ggamini7@gmail.com>
2022-11-19 02:16:34 +0900

    Cos pro 소수의 세제곱이 몇개가 있나요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -4,28 +4,50 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public String solution(int hour, int minute) {
-        double hourAngle = 30 * hour + 0.5 * minute;
-        double minuteAngle = 6 * minute;
-        double angle = 0;
-        if (hourAngle >= minuteAngle) {
-            angle = hourAngle - minuteAngle;
-        } else {
-            angle = minuteAngle - hourAngle;
+    public int solution(int a, int b) {
+        ArrayList<Integer> zegob = new ArrayList<>();
+        int i = 2;
+        while (Math.pow(i, 2) <= b) {
+            if (a <= Math.pow(i, 2)) {
+                if (isPrime(i))
+                    zegob.add((int)Math.pow(i, 2));
+            }
+            i++;
+        }
+        ArrayList<Integer> saezegob = new ArrayList<>();
+        i = 2;
+        while (Math.pow(i, 3) <= b) {
+            if (a <= Math.pow(i, 3)) {
+                if (isPrime(i))
+                    saezegob.add((int)Math.pow(i, 3));
+            }
+            i++;
         }
 
-        String answer = Double.toString(angle);
-        return answer;
+        return zegob.size() + saezegob.size();
+    }
+
+    public boolean isPrime(int num) {
+        int cnt = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                cnt++;
+            }
+        }
+        if (cnt > 2)
+            return false;
+        else
+            return true;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Main sol = new Main();
-        int hour = 3;
-        int minute = 0;
-        String ret = sol.solution(hour, minute);
+        int a = 6;
+        int b = 30;
+        int ret = sol.solution(a, b);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
     }
 }
