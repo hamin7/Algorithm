fd8de67869d533c3d7cf3a5dc1e5f6befec413e8
hamin <ggamini7@gmail.com>
2022-11-18 18:31:54 +0900

    Cos pro 단어를 순서대로 적으세요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -4,24 +4,29 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public int solution(int[] arr, int K) {
-        Arrays.sort(arr);
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < arr.length - K + 1; i++) {
-            int gap = arr[i+K-1] - arr[i];
-            min = Math.min(min, gap);
+    public int solution(int K, String[] words) {
+        int line = 0;
+        int space = 0;
+        int idx = 0;
+        while (idx < words.length) {
+            if (space + words[idx].length() + 1 < K) {
+                space += words[idx].length();
+                idx++;
+            } else {
+                line++;
+                space = 0;
+            }
         }
 
-        return min;
+        return line;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] arr = {9, 11, 9, 6, 4, 19};
-        int K = 4;
-        int ret = sol.solution(arr, K);
+        int K = 10;
+        String[] words = {new String("nice"), new String("happy"), new String("hello"), new String("world"), new String("hi")};
+        int ret = sol.solution(K, words);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
