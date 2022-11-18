9296a9f4e4c4670be2d2751d3d63642a78a2ba67
hamin <ggamini7@gmail.com>
2022-11-18 17:17:45 +0900

    Cos pro 카드 섞기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,32 +1,44 @@
 package problemSolving;
 
 class Main {
-    public int solution(int[][] board) {
+    public int solution(int n, int mix, int k) {
         int answer = 0;
 
-        int[][] coins = new int[4][4];
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                if(i == 0 && j == 0)
-                    coins[i][j] = board[i][j];
-                else if(i == 0 && j != 0)
-                    coins[i][j] = board[i][j] + coins[i][j-1];
-                else if(i != 0 && j == 0)
-                    coins[i][j] = board[i][j] + coins[i-1][j];
+        int[] card = new int[n];
+        for(int i = 0; i < n; i++)
+            card[i] = i+1;
+
+        while((mix--) != 0) {
+            int[] cardA = new int[n/2];
+            int[] cardB = new int[n/2];
+
+            for(int i = 0; i < n; i++) {
+                if(i < n/2)
+                    cardA[i] = card[i];
                 else
-                    coins[i][j] = board[i][j] + Math.max(coins[i][j-1], coins[i-1][j]);
+                    cardB[i - n/2] = card[i];
+            }
+
+            for(int i = 0; i < n; i++) {
+                if(i % 2 == 0)
+                    card[i] = cardA[i/2];
+                else
+                    card[i] = cardB[i/2];
             }
         }
 
-        answer = coins[3][3];
+        answer = card[k-1];
+
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. 아래에는 잘못된 부분이 없으니 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[][] board = {{6, 7, 1, 2}, {3, 5, 3, 9}, {6, 4, 5, 2}, {7, 3, 2, 6}};
-        int ret = sol.solution(board);
+        int n = 6;
+        int mix = 3;
+        int k = 3;
+        int ret = sol.solution(n, mix, k);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
