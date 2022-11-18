8cac6232b4569edeb6068dbe26e9ae27fc8d6878
hamin <ggamini7@gmail.com>
2022-11-18 21:39:09 +0900

    Cos pro 물을 최대로 담고 싶어요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,46 +1,32 @@
 package problemSolving;
 
-import java.util.*;
-
 class Main {
-    public void swap(int[] numbers, int i, int j) {
-        int tmp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = tmp;
-    }
+    public int solution(int[][] walls) {
+        int answer = 0;
 
-    public int[] solution(int[] numbers) {
-        int[] answer = {};
+        for(int i = 0; i < walls.length; i++) {
+            for(int j = i+1; j < walls.length; j++) {
+                int area = 0;
+                if(walls[i][1] > walls[j][1])
+                    area = walls[j][1] * (walls[j][0] - walls[i][0]);
+                else
+                    area = walls[i][1] * (walls[j][0] - walls[i][0]);
 
-        Arrays.sort(numbers);
-        int mid = (numbers.length - 1) / 2;
-        swap(numbers, mid, numbers.length-1);
-
-        int left = mid + 1;
-        int right = numbers.length - 2;
-        while(left <= right) {
-            swap(numbers, left, right);
-            left = left + 1;
-            right = right - 1;
+                if(answer < area)
+                    answer = area;
+            }
         }
 
-        answer = numbers;
         return answer;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. 아래에는 잘못된 부분이 없으니 위의 코드만 수정하세요.
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] numbers = {7, 3, 4, 1, 2, 5, 6};
-        int[] ret = sol.solution(numbers);
+        int[][] walls = {{1, 4}, {2, 6}, {3, 5}, {5, 3}, {6, 2}};
+        int ret = sol.solution(walls);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.print("solution 메소드의 반환 값은 [");
-        for(int i = 0; i < ret.length; i++) {
-            if(i != 0)
-                System.out.print(", ");
-            System.out.print(ret[i]);
-        }
-        System.out.println("] 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
     }
 }
