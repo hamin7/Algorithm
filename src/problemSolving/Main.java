b090b7b2c6e373393bca481ab75a64b4d051dca8
hamin <ggamini7@gmail.com>
2022-11-19 01:56:12 +0900

    Cos pro 분침과 시침의 각도 구하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,85 +1,31 @@
 package problemSolving;
 
-import java.util.Arrays;
+// 다음과 같이 import를 사용할 수 있습니다.
+import java.util.*;
 
 class Main {
-    class Unit {
-        public int HP;
-        public Unit() {
-            this.HP = 1000;
+    public String solution(int hour, int minute) {
+        double hourAngle = 30 * hour + 0.5 * minute;
+        double minuteAngle = 6 * minute;
+        double angle = 0;
+        if (hourAngle >= minuteAngle) {
+            angle = hourAngle - minuteAngle;
+        } else {
+            angle = minuteAngle - hourAngle;
         }
-        public void underAttack(int damage) { }
-    }
 
-    class Monster extends Unit {
-        public int attackPoint;
-        public Monster(int attackPoint) {
-            this.attackPoint = attackPoint;
-        }
-        public void underAttack(int damage) {
-            this.HP -= damage;
-        }
-        public int attack() {
-            return attackPoint;
-        }
-    }
-
-    class Warrior extends Unit {
-        public int attackPoint;
-        public Warrior(int attackPoint) {
-            this.attackPoint = attackPoint;
-        }
-        public void underAttack(int damage) {
-            this.HP -= damage;
-        }
-        public int attack() {
-            return attackPoint;
-        }
-    }
-
-    class Healer extends Unit {
-        public int healingPoint;
-        public Healer(int healingPoint) {
-            this.healingPoint = healingPoint;
-        }
-        public void underAttack(int damage) {
-            this.HP -= damage;
-        }
-        public void healing(Unit unit) {
-            unit.HP += healingPoint;
-        }
-    }
-    public int[] solution(int monsterAttackPoint, int warriorAttackPoint, int healingPoint) {
-        Monster monster = new Monster(monsterAttackPoint);
-        Warrior warrior = new Warrior(warriorAttackPoint);
-        Healer healer = new Healer(healingPoint);
-
-        //전사가 몬스터를 한 번 공격
-        monster.underAttack(warrior.attack());
-        //몬스터가 전사를 한 번 공격
-        warrior.underAttack(monster.attack());
-        //몬스터가 힐러를 한 번 공격
-        healer.underAttack(monster.attack());
-        //힐러가 전사의 체력을 한 번 회복
-        healer.healing(warrior);
-        //힐러가 몬스터의 체력을 한 번 회복
-        healer.healing(monster);
-
-        int[] answer = {monster.HP, warrior.HP, healer.HP};
+        String answer = Double.toString(angle);
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int monsterAttackPoint = 100;
-        int warriorAttackPoint = 90;
-        int healingPoint = 30;
-        int[] ret = sol.solution(monsterAttackPoint, warriorAttackPoint, healingPoint);
+        int hour = 3;
+        int minute = 0;
+        String ret = sol.solution(hour, minute);
 
-        // 실행] 버튼을 누르면 출력값을 볼 수 있습니다.
-        System.out.printf("solution 메소드의 반환 값은 ");
-        System.out.printf(Arrays.toString(ret));
-        System.out.printf(" 입니다.\n");
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
     }
 }
