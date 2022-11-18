fcbf19018b3dbda3d3b0a5f78104c88fba158292
hamin <ggamini7@gmail.com>
2022-11-18 21:28:38 +0900

    Cos pro 각 숫자가 몇개가 있나요

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,40 +1,35 @@
 package problemSolving;
 
-// 다음과 같이 import를 사용할 수 있습니다.
-import java.util.*;
-
 class Main {
-    public int solution(int[] enemies, int[] armies) {
-        Arrays.sort(enemies);
-        Arrays.sort(armies);
-        int answer = 0;
-        int armiesCnt = armies.length - 1;
+    public String solution(int number) {
+        String answer = "";
 
-        for (int i = 0; i < enemies.length; i++) {
-            if (enemies[enemies.length - i - 1] <= armies[armiesCnt]) {
-                answer++;
-                armiesCnt--;
-            }
+        int[] numberCount = new int[10];
+        while(number > 0) {
+            numberCount[number % 10]++;
+            number /= 10;
         }
+
+        for(int i = 9; i >= 0; i--)
+            if(numberCount[i] != 0)
+                answer += (String.valueOf(i) + String.valueOf(numberCount[i]));
 
         return answer;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] enemies1 = {1, 4, 3};
-        int[] armies1 = {1, 3};
-        int ret1 = sol.solution(enemies1, armies1);
+        int number1 = 2433;
+        String ret1 = sol.solution(number1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 \"" + ret1 + "\" 입니다.");
 
-        int[] enemies2 = {1, 1, 1};
-        int[] armies2 = {1, 2, 3, 4};
-        int ret2 = sol.solution(enemies2, armies2);
+        int number2 = 662244;
+        String ret2 = sol.solution(number2);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 \"" + ret2 + "\" 입니다.");
     }
 }
