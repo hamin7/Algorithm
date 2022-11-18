d3ce3dfad15823b5c5ba9e5308a198d3eed9934d
hamin <ggamini7@gmail.com>
2022-11-18 21:58:43 +0900

    Cos pro 사전에서 단어찾기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,31 +1,43 @@
 package problemSolving;
 
-class Main {
-    public int solution(int n) {
+import java.util.*;
+
+public class Main {
+    String[] vowels = {"A", "E", "I", "O", "U"};
+    ArrayList<String> words;
+    public void create_words(int lev, String str) {
+        words.add(str);
+        for (int i = 0; i < 5; i++) {
+            if (lev < 5) {
+                create_words(lev+1, str.concat(vowels[i]));
+            }
+        }
+    }
+
+    public int solution(String word) {
         int answer = 0;
-
-        int[] steps = new int[n+1];
-        steps[1] = 1;
-        steps[2] = 2;
-        steps[3] = 4;
-        for(int i = 4; i <= n; i++)
-            steps[i] = steps[i-1] + steps[i-2] + steps[i-3];
-        answer = steps[n];
-
+        words  = new ArrayList<String>();
+        create_words(0, "");
+        for (int i = 0; i < words.size(); i++) {
+            if (word.equals(words.get(i))) {
+                answer = i;
+                break;
+            }
+        }
         return answer;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    // 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        int n1 = 3;
-        int ret1 = sol.solution(n1);
+        String word1 = new String("AAAAE");
+        int ret1 = sol.solution(word1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
 
-        int n2 = 4;
-        int ret2 = sol.solution(n2);
+        String word2 = new String("AAAE");
+        int ret2 = sol.solution(word2);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
