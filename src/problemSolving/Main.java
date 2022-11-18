1a8383af9425d0b35a8b579faca2703f217e8635
hamin <ggamini7@gmail.com>
2022-11-18 22:06:49 +0900

    Cos pro 스키장 최소 비용 구하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,36 +1,33 @@
 package problemSolving;
 
-import java.util.*;
-
 class Main {
-    public String solution(String s) {
-        s = s.toLowerCase();
-        String answer = "";
-        char previous = s.charAt(0);
-        int counter = 1;
-
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i) == previous)
-                counter++;
-            else {
-                answer += previous;
-                answer += counter;
-                counter = 1;
-                previous = s.charAt(i+1);
-            }
-        }
-        answer += previous;
-        answer += counter;
-        return answer;
+    public long solution(int oneDayPrice, int multiDay, int multiDayPrice, long n){
+        if(oneDayPrice * multiDay <= multiDayPrice)
+            return n * oneDayPrice;
+        else
+            return (n % multiDay) * oneDayPrice + (n / multiDay) * multiDayPrice;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        String s = new String("YYYYYbbbBbbBBBMmmM");
-        String ret = sol.solution(s);
+
+        int oneDayPrice1 = 3;
+        int multiDay1 = 5;
+        int multiDayPrice1 = 14;
+        long n1 = 6;
+        long ret1 = sol.solution(oneDayPrice1, multiDay1, multiDayPrice1, n1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+        int oneDayPrice2 = 2;
+        int multiDay2 = 3;
+        int multiDayPrice2 = 5;
+        long n2 = 11;
+        long ret2 = sol.solution(oneDayPrice2, multiDay2, multiDayPrice2, n2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
