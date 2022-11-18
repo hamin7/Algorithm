6809e018a3f62a44572b0bdb080a6c54e97aeb0b
hamin <ggamini7@gmail.com>
2022-11-18 22:02:26 +0900

    Cos pro 문자열 압축

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -2,44 +2,35 @@ package problemSolving;
 
 import java.util.*;
 
-public class Main {
-    String[] vowels = {"A", "E", "I", "O", "U"};
-    ArrayList<String> words;
-    public void create_words(int lev, String str) {
-        words.add(str);
-        for (int i = 0; i < 5; i++) {
-            if (lev < 5) {
-                create_words(lev+1, str.concat(vowels[i]));
-            }
-        }
-    }
+class Main {
+    public String solution(String s) {
+        s = s.toLowerCase();
+        String answer = "";
+        char previous = s.charAt(0);
+        int counter = 1;
 
-    public int solution(String word) {
-        int answer = 0;
-        words  = new ArrayList<String>();
-        create_words(0, "");
-        for (int i = 0; i < words.size(); i++) {
-            if (word.equals(words.get(i))) {
-                answer = i;
-                break;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i) == previous)
+                counter++;
+            else {
+                answer += previous;
+                answer += counter;
+                counter = 1;
+                previous = s.charAt(i+1);
             }
         }
+        answer += previous;
+        answer += counter;
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
     public static void main(String[] args) {
         Main sol = new Main();
-        String word1 = new String("AAAAE");
-        int ret1 = sol.solution(word1);
+        String s = new String("YYYYYbbbBbbBBBMmmM");
+        String ret = sol.solution(s);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
-
-        String word2 = new String("AAAE");
-        int ret2 = sol.solution(word2);
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
     }
 }
