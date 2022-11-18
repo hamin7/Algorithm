a82e7b5d822202b5ac68c4de20d04c652c9f0275
hamin <ggamini7@gmail.com>
2022-11-18 19:23:51 +0900

    Cos pro 그래프에서 싸이클찾기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,41 +1,37 @@
 package problemSolving;
 
 class Main {
-    public int func_a(int a, int b) {
-        int mod = a % b;
+    public int find(int[] parent, int u) {
+        if(u == parent[u])
+            return u;
 
-        while(mod > 0) {
-            a = b;
-            b = mod;
-            mod = a % b;
-        }
-
-        return b;
+        parent[u] = find(parent, parent[u]);
+        return parent[u];
     }
 
-    public int func_b(int n) {
-        int answer = 0;
+    public boolean merge(int[] parent, int u, int v) {
+        u = find(parent, u);
+        v = find(parent, v);
 
-        for(int i = 1; i <= n; i++) {
-            if(func_c(n, i))
-                answer++;
-        }
-
-        return answer;
-    }
-
-    public boolean func_c(int p, int q) {
-        if(p % q == 0)
+        if(u == v)
             return true;
-        else
-            return false;
+
+        parent[u] = v;
+        return false;
     }
 
-    public int solution(int a, int b, int c) {
+    public int solution(int n, int[][] connections) {
         int answer = 0;
 
-        int gcd = func_a(func_a(a, b), c);
-        answer = func_b(gcd);
+        int[] parent = new int[n+1];
+        for(int i = 1; i <= n; i++)
+            parent[i] = i;
+
+        for(int i = 0; i < connections.length; i++)
+            if(merge(parent, connections[i][0], connections[i][1])) {
+                answer = i + 1;
+                break;
+            }
 
         return answer;
     }
@@ -43,10 +39,9 @@ class Main {
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int a = 24;
-        int b = 9;
-        int c = 15;
-        int ret = sol.solution(a, b, c);
+        int n = 3;
+        int[][] connections = {{1, 2}, {1, 3}, {2, 3}};
+        int ret = sol.solution(n, connections);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
