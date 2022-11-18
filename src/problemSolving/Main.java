4dd95a0f5e7fcfb2eb2f0fbf7b04efde8f55cb71
hamin <ggamini7@gmail.com>
2022-11-18 19:10:23 +0900

    Cos pro 공약수 구하기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,67 +1,41 @@
 package problemSolving;
 
 class Main {
-    class Job {
-        public int salary;
+    public int func_a(int a, int b) {
+        int mod = a % b;
 
-        public Job() {
-            this.salary = 0;
+        while(mod > 0) {
+            a = b;
+            b = mod;
+            mod = a % b;
         }
 
-        public int getSalary() {
-            return salary;
-        }
+        return b;
     }
 
-    class PartTimeJob extends Job {
-        public int workHour, payPerHour;
-
-        public PartTimeJob(int workHour, int payPerHour) {
-            this.workHour = workHour;
-            this.payPerHour = payPerHour;
-        }
-
-        public int getSalary() {
-            salary = workHour * payPerHour;
-            if(workHour >= 40)
-                salary += (payPerHour * 8);
-
-            return salary;
-        }
-    }
-
-    class SalesJob extends Job {
-        public int salesResult, payPerSale;
-
-        public SalesJob(int salesResult, int payPerSale) {
-            this.salesResult = salesResult;
-            this.payPerSale = payPerSale;
-        }
-
-        public int getSalary() {
-            if(salesResult > 20)
-                salary = salesResult * payPerSale * 3;
-            else if(salesResult > 10)
-                salary = salesResult * payPerSale * 2;
-            else
-                salary = salesResult * payPerSale;
-
-            return salary;
-        }
-    }
-
-    public int solution(int[][] partTimeJobs, int[][] salesJobs) {
+    public int func_b(int n) {
         int answer = 0;
 
-        for(int i = 0; i < partTimeJobs.length; i++) {
-            PartTimeJob partTimeJob = new PartTimeJob(partTimeJobs[i][0], partTimeJobs[i][1]);
-            answer += partTimeJob.getSalary();
+        for(int i = 1; i <= n; i++) {
+            if(func_c(n, i))
+                answer++;
         }
 
-        for(int i = 0; i < salesJobs.length; i++) {
-            SalesJob salesJob = new SalesJob(salesJobs[i][0], salesJobs[i][1]);
-            answer += salesJob.getSalary();
-        }
+        return answer;
+    }
+
+    public boolean func_c(int p, int q) {
+        if(p % q == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        int gcd = func_a(func_a(a, b), c);
+        answer = func_b(gcd);
 
         return answer;
     }
@@ -69,9 +43,10 @@ class Main {
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[][] partTimeJobs = {{10, 5000}, {43, 6800}, {5, 12800}};
-        int[][] salesJobs = {{3, 18000}, {12, 8500}};
-        int ret = sol.solution(partTimeJobs, salesJobs);
+        int a = 24;
+        int b = 9;
+        int c = 15;
+        int ret = sol.solution(a, b, c);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
