b073a209b5bdf8a067bf52ae74495e2f06bd01cb
hamin <ggamini7@gmail.com>
2022-11-18 17:44:41 +0900

    Cos pro 큰수와 작은수의 차이

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -1,44 +1,27 @@
 package problemSolving;
 
+// 다음과 같이 import를 사용할 수 있습니다.
+import java.util.*;
+
 class Main {
-    public int solution(int n, int mix, int k) {
-        int answer = 0;
+    public int solution(int[] arr, int K) {
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
 
-        int[] card = new int[n];
-        for(int i = 0; i < n; i++)
-            card[i] = i+1;
-
-        while((mix--) != 0) {
-            int[] cardA = new int[n/2];
-            int[] cardB = new int[n/2];
-
-            for(int i = 0; i < n; i++) {
-                if(i < n/2)
-                    cardA[i] = card[i];
-                else
-                    cardB[i - n/2] = card[i];
-            }
-
-            for(int i = 0; i < n; i++) {
-                if(i % 2 == 0)
-                    card[i] = cardA[i/2];
-                else
-                    card[i] = cardB[i/2];
-            }
+        for (int i = 0; i < arr.length - K + 1; i++) {
+            int gap = arr[i+K-1] - arr[i];
+            min = Math.min(min, gap);
         }
 
-        answer = card[k-1];
-
-        return answer;
+        return min;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다. 아래에는 잘못된 부분이 없으니 위의 코드만 수정하세요.
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int n = 6;
-        int mix = 3;
-        int k = 3;
-        int ret = sol.solution(n, mix, k);
+        int[] arr = {9, 11, 9, 6, 4, 19};
+        int K = 4;
+        int ret = sol.solution(arr, K);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
