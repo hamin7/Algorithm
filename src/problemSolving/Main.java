56616c3f4e91bcfa86be8af1bf7cd40f280c3431
hamin <ggamini7@gmail.com>
2022-11-18 21:18:39 +0900

    Cos pro 몬스터 잡기

--- src/problemSolving/Main.java
+++ src/problemSolving/Main.java
@@ -4,33 +4,37 @@ package problemSolving;
 import java.util.*;
 
 class Main {
-    public String solution(String s1, String s2, int p, int q) {
-        int s1Ten = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            int num = s1.charAt(s1.length() - i - 1) - '0';
-            s1Ten += num * (int) Math.pow(p, i);
+    public int solution(int[] enemies, int[] armies) {
+        Arrays.sort(enemies);
+        Arrays.sort(armies);
+        int answer = 0;
+        int armiesCnt = armies.length - 1;
+
+        for (int i = 0; i < enemies.length; i++) {
+            if (enemies[enemies.length - i - 1] <= armies[armiesCnt]) {
+                answer++;
+                armiesCnt--;
+            }
         }
 
-        int s2Ten = 0;
-        for (int i = 0; i < s2.length(); i++) {
-            int num = s2.charAt(s2.length() - i - 1) - '0';
-            s2Ten += num * (int) Math.pow(p, i);
-        }
-
-        String answer = Integer.toOctalString(s1Ten+s2Ten);
         return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        String s1 = new String("112001");
-        String s2 = new String("12010");
-        int p = 3;
-        int q = 8;
-        String ret = sol.solution(s1, s2, p, q);
+        int[] enemies1 = {1, 4, 3};
+        int[] armies1 = {1, 3};
+        int ret1 = sol.solution(enemies1, armies1);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+        int[] enemies2 = {1, 1, 1};
+        int[] armies2 = {1, 2, 3, 4};
+        int ret2 = sol.solution(enemies2, armies2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
