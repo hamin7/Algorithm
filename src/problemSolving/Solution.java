0d4d48982e12d1a69dedbeb22cb40d3d5eac4321
hamin <ggamini7@gmail.com>
2022-12-05 21:03:08 +0900

    add 프로그래머스 귤 고르기

--- src/problemSolving/Solution.java
+++ src/problemSolving/Solution.java
@@ -1,56 +1,44 @@
 package problemSolving;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Solution {
-    public double[] solution(int k, int[][] ranges) {
-        double[] answer = new double[ranges.length];
-        ArrayList<Double> sequence = new ArrayList<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
 
-        double index = 0;
-        sequence.add((double) k);
-        while (k != 1) {
-            index++;
-            if (k % 2 == 0) {
-                k /= 2;
-            } else {
-                k *= 3;
-                k++;
-            }
-            sequence.add((double) k);
+    public int solution(int k, int[] tangerine) {
+
+        for (int each : tangerine) {
+            map.put(each, map.getOrDefault(each, 0) + 1);
         }
 
-        ArrayList<Double> sizes = new ArrayList<>();
-        for (int i = 0; i < sequence.size() - 1; i++) {
-            sizes.add((sequence.get(i) + sequence.get(i+1)) / 2);
+        List<Integer> quantityList = mapToList(map);
+        quantityList.sort(Comparator.reverseOrder());
+
+        return packaging(quantityList, k);
+    }
+
+    public List<Integer> mapToList(HashMap<Integer, Integer> map) {
+        return new ArrayList<>(map.values());
+    }
+
+    public int packaging(List<Integer> quantityList, int k) {
+        int cnt = 0;
+        int idx = 0;
+        while (k > 0) {
+            k -= quantityList.get(idx);
+            idx++;
+            cnt++;
         }
-
-        for (int i = 0; i < ranges.length; i++) {
-            int left = ranges[i][0];
-            int right = sequence.size() - 1 + ranges[i][1];
-
-            double area = 0;
-
-            if (left <= right) {
-                for (int j = left; j < right; j++) {
-                    area += sizes.get(j);
-                }
-            } else
-                area = -1;
-
-            answer[i] = area;
-        }
-
-        return answer;
+        return cnt;
     }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        int k = 5;
-        int[][] arr = {{0,0}, {0,-1}, {2,-3}, {3,-3}};
-        double[] ret1 = sol.solution(k, arr);
+        int k = 6;
+        int[] arr = {1, 3, 2, 5, 4, 5, 2, 3};
+        int ret1 = sol.solution(k, arr);
 
-        System.out.println("solution 메소드의 반환 값은 \"" + ret1.toString() + "\" 입니다.");
+        System.out.println("solution 메소드의 반환 값은 \"" + ret1 + "\" 입니다.");
     }
 }
