52bbbdec9f1e9933a5b6ba91b0d86a738c22997f
hamin <ggamini7@gmail.com>
2022-12-13 15:36:33 +0900

    add 프로그래머스 문자열 나누기

--- src/problemSolving/Solution.java
+++ src/problemSolving/Solution.java
@@ -1,43 +1,36 @@
 package problemSolving;
 
-import java.util.*;
 
 public class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    public int solution(String s) {
+        int answer = 0, sameCnt = 0, diffCnt = 0;
+        char firstLetter = s.charAt(0);
 
-    public int solution(int k, int[] tangerine) {
-
-        for (int each : tangerine) {
-            map.put(each, map.getOrDefault(each, 0) + 1);
+        for (int i = 0; i < s.length(); i++) {
+            if (firstLetter == s.charAt(i))
+                sameCnt++;
+            else
+                diffCnt++;
+            if (i == s.length() - 1) {
+                answer++;
+                break;
+            }
+            if (sameCnt == diffCnt) {
+                answer++;
+                firstLetter = s.charAt(i+1);
+                sameCnt = 0;
+                diffCnt = 0;
+            }
         }
 
-        List<Integer> quantityList = mapToList(map);
-        quantityList.sort(Comparator.reverseOrder());
-
-        return packaging(quantityList, k);
-    }
-
-    public List<Integer> mapToList(HashMap<Integer, Integer> map) {
-        return new ArrayList<>(map.values());
-    }
-
-    public int packaging(List<Integer> quantityList, int k) {
-        int cnt = 0;
-        int idx = 0;
-        while (k > 0) {
-            k -= quantityList.get(idx);
-            idx++;
-            cnt++;
-        }
-        return cnt;
+        return answer;
     }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        int k = 6;
-        int[] arr = {1, 3, 2, 5, 4, 5, 2, 3};
-        int ret1 = sol.solution(k, arr);
+        String s = "banana";
+        int ret1 = sol.solution(s);
 
         System.out.println("solution 메소드의 반환 값은 \"" + ret1 + "\" 입니다.");
     }
