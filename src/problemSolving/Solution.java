248ea0bfc9fa0f51e22e1204df8ee86d7042d1a3
hamin <ggamini7@gmail.com>
2023-02-15 00:39:24 +0900

    add 프로그래머스 호텔 대실

--- src/problemSolving/Solution.java
+++ src/problemSolving/Solution.java
@@ -3,82 +3,69 @@ package problemSolving;
 import java.util.*;
 
 public class Solution {
-    public int solution(int x, int y, int n) {
-        int result = 1;
-        int currentNode = x;
-        Set<Integer> nodeSet = new HashSet<>();
 
-        // 처음에 x가 y와 같으면 0 리턴
-        if (x == y) return 0;
+    class Book {
+        int start_time, end_time;
 
-        // 처음 nodeSet 만들기
-        // 연산 1
-        currentNode = x + n;
-        if (currentNode < y)
-            nodeSet.add(currentNode);
-        else if (currentNode == y)
-            return 1;
+        public Book(int start_time, int end_time) {
+            this.start_time = start_time;
+            this.end_time = end_time;
+        }
+    }
 
-        // 연산 2
-        currentNode = x * 3;
-        if (currentNode < y)
-            nodeSet.add(currentNode);
-        else if (currentNode == y)
-            return 1;
+    List<Book> bookList = new ArrayList<>();
 
-        // 연산 3
-        currentNode = x * 2;
-        if (currentNode < y)
-            nodeSet.add(currentNode);
-        else if (currentNode == y)
-            return 1;
+    int toMinute(String time) {
+        StringTokenizer stk = new StringTokenizer(time, ":");
+        int hour = Integer.parseInt(stk.nextToken());
+        int minute = Integer.parseInt(stk.nextToken());
+        return hour * 60 * minute;
+    }
 
-        // nodeSet에서 node 확장
-        mainLoop:
-        while (true) {
-            result++;
-            Set<Integer> newNodeSet = new HashSet<>();
+    public int solution(String[][] book_times) {
+        for (String[] book_time : book_times) {
+            int start_time = toMinute(book_time[0]);
+            int end_time = toMinute(book_time[1]);
 
-            for (int parentNode : nodeSet) {
-                // 연산 1
-                currentNode = parentNode + n;
-                if (currentNode < y)
-                    newNodeSet.add(currentNode);
-                else if (currentNode == y)
-                    break mainLoop;
+            bookList.add(new Book(start_time, end_time));
+        }
+        // 시작 시간대로 정렬
+        Collections.sort(bookList, (o1, o2) -> {
+            if (o1.start_time == o2.start_time) return o1.end_time - o2.end_time;
+            else return o1.start_time - o2.start_time;
+        });
 
-                // 연산 2
-                currentNode = parentNode * 3;
-                if (currentNode < y)
-                    newNodeSet.add(currentNode);
-                else if (currentNode == y)
-                    break mainLoop;
+        // 리스트를 방문하며 몇개가 필요할지 체크
+        List<Integer> endTimeList = new ArrayList<>();
 
-                // 연산 3
-                currentNode = parentNode * 2;
-                if (currentNode < y)
-                    newNodeSet.add(currentNode);
-                else if (currentNode == y)
-                    break mainLoop;
+        for (Book book : bookList) {
+            boolean isOk = false;
+            // end_time 기준으로 정렬
+            Collections.sort(endTimeList);
+            for (int i = 0; i < endTimeList.size(); i++) {
+                // 정리시간 10분
+                int endTime = endTimeList.get(i) + 10;
+                // check
+                if (book.start_time >= endTime) {
+                    // 예약 시간 넣고 업데이트
+                    endTimeList.set(i, book.end_time);
+                    isOk = true;
+                    break;
+                }
             }
-
-            // 새로 생긴 노드가 없다면 -1을 리턴
-            if (newNodeSet.size() == 0) return -1;
-
-            // nodeSet 최신화
-            nodeSet = newNodeSet;
+            // 아무 방도 넣지 못하면 새로운 방 추가
+            if (!isOk) endTimeList.add(book.end_time);
         }
 
-        return result;
+        return endTimeList.size();
     }
 
     public static void main(String[] args) {
 
         Solution sol = new Solution();
-        int x = 10;
-        int y = 40;
-        int n = 5;
-        long result = sol.solution(x, y, n);
+        String[][] book_time = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
+
+        long result = sol.solution(book_time);
 
         System.out.println("solution 메소드의 반환 값은 \"" + result + "\" 입니다.");
     }
