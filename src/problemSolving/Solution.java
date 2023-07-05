0456b9aced14c5114d82d73ec7af9cc9285a56f8
hamin <ggamini7@gmail.com>
2023-07-05 16:31:51 +0900

    add 백준 20437 문자열 게임 2

--- src/problemSolving/Solution.java
+++ src/problemSolving/Solution.java
@@ -1,97 +1,47 @@
 package problemSolving;
 
 import java.io.*;
-import java.util.*;
-
-class Town implements Comparable<Town> {
-    int end;
-    int weight;
-
-    Town(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Town arg0) {
-        return weight - arg0.weight;
-    }
-}
 
 public class Solution {
-    static final int INF = 987654321;
-    static ArrayList<ArrayList<Town>> arrList, reverse_arrList;
-    static int N, X;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/leehamin/Algorithm/src/problemSolving/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        //입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < t; i++) {
+            String str = bf.readLine();
+            int k = Integer.parseInt(bf.readLine());
+            //입력 끝
 
-        arrList = new ArrayList<>(); // 문제의 입력을 그대로 받은 배열
-        reverse_arrList = new ArrayList<>(); // 문제의 입력을 반대로 받은 배열
+            if (k == 1) { //k가 1일때
+                System.out.println("1 1");
+                continue;
+            }
 
-        for (int i = 0; i <= N; i++) {
-            arrList.add(new ArrayList<>());
-            reverse_arrList.add(new ArrayList<>());
-        }
+            int[] alpha = new int[26];//알파벳 별 개수를 저장한다.
+            for (int j = 0; j < str.length(); j++) {
+                alpha[str.charAt(j) - 'a']++;
+            }
 
-        // arrList와 reverse_arrList를 각각 단방향 인접리스트로 구현
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int min = Integer.MAX_VALUE;
+            int max = -1;
+            for (int j = 0; j < str.length(); j++) {
+                if (alpha[str.charAt(j) - 'a'] < k) continue;
 
-            arrList.get(start).add(new Town(end, weight));
-            reverse_arrList.get(end).add(new Town(start, weight));
-        }
-
-        int[] dist1 = dijkstra(arrList); // X에서 시작점들 사이의 최단거리
-        int[] dist2 = dijkstra(reverse_arrList); // 시작점들에서 X 사이의 최단거리
-
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            ans = Math.max(ans, dist1[i] + dist2[i]);
-        }
-
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    // 다익스트라 알고리즘
-    public static int[] dijkstra(ArrayList<ArrayList<Town>> a) {
-        PriorityQueue<Town> pq = new PriorityQueue<>();
-        pq.offer(new Town(X, 0));
-
-        boolean[] check = new boolean[N + 1];
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, INF);
-        dist[X] = 0;
-
-        while (!pq.isEmpty()) {
-            Town curTown = pq.poll();
-            int cur = curTown.end;
-
-            if (!check[cur]) {
-                check[cur] = true;
-
-                for (Town town : a.get(cur)) {
-                    if (!check[town.end] && dist[town.end] > dist[cur] + town.weight) {
-                        dist[town.end] = dist[cur] + town.weight;
-                        pq.add(new Town(town.end, dist[town.end]));
+                int count = 1;
+                for (int l = j + 1; l < str.length(); l++) {
+                    if (str.charAt(j) == str.charAt(l)) count++;
+                    if (count == k) {
+                        min = Math.min(min, l - j + 1);
+                        max = Math.max(max, l - j + 1);
+                        break;
                     }
                 }
             }
+            if (min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
+            else System.out.println(min + " " + max);
         }
-        return dist;
     }
-
 }
